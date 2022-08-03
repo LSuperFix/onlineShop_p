20c0d4104fb9e48575d595d49dbb5380d1174f5a
LSuperFix <buspart3@gmail.com>
2022-08-03 15:43:57 +0300

    search optimisation with debonce

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,12 +1,25 @@
 import styles from './search.module.scss'
 import iconSearch from "../../assets/img/211818_search_icon.svg"
 import iconExit from "../../assets/img/4781838_cancel_close_delete_exit_logout_icon.svg"
-import { useRef } from 'react'
+import { useState, useCallback, useRef } from 'react'
+import debounce from 'lodash.debounce'
 
-function Search({searchValue, setSearchValue}) {
+function Search({setSearchValue}) {
+  const [searchValueLocal, setSearchValueLocal] = useState('')
+  const updateSearchValue = useCallback(
+    debounce((str) => {
+      setSearchValue(str)
+    }, 250), [])
+
+  function onChangeInput(event) {
+    setSearchValueLocal(event.target.value)
+    updateSearchValue(event.target.value)
+  }
+
   const inputRef = useRef()
   function cleanSearchValue() {
     setSearchValue('')
+    setSearchValueLocal('')
     inputRef.current.focus()
   }
   return(
@@ -15,11 +28,11 @@ function Search({searchValue, setSearchValue}) {
       <input 
         className={styles.input} 
         placeholder='Поиск'
-        value={searchValue} 
+        value={searchValueLocal} 
         ref={inputRef}
-        onChange={(e)=>setSearchValue(e.target.value)}
+        onChange={(e)=>onChangeInput(e)}
       />
-      {searchValue&&<img className={styles.iconExit} onClick={() => cleanSearchValue()} src={iconExit}/>}
+      {searchValueLocal&&<img className={styles.iconExit} onClick={() => cleanSearchValue()} src={iconExit}/>}
     </div>
     
   )
